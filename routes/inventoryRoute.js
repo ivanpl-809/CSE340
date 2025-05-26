dcdb870c3f97ac2bfc79cc9ed1c53eb723ffa2df
ivan-pl809 <pon20007@byui.edu>
2025-05-26 13:37:34 -0600

    commit

--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -3,7 +3,7 @@ const router = new express.Router()
 const invController = require("../controllers/inventoryController")
 
 // Route to build inventory by classification view
-router.get("/type/:classificationId", invController.buildByClassificationId);
+router.get("/type/:classificationId", inventoryController.buildByClassificationId);
 router.get('/detail/:inv_id', inventoryController.buildVehicleDetail);
 
 
