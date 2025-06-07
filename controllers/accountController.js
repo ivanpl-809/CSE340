253cd404fc4d682650aba93e657d192fc1e50a5f
ivan-pl809 <pon20007@byui.edu>
2025-06-07 17:53:50 -0600

    commit

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -1,5 +1,7 @@
 const utilities = require("../utilities")
 const accountModel = require("../models/account-model")
+const bcrypt = require("bcryptjs")
+
 
 
 /* ****************************************
@@ -26,6 +28,8 @@ async function buildRegister(req, res, next) {
   })
 }
 
+
+
 /* ****************************************
 *  Process Registration
 * *************************************** */
@@ -33,11 +37,25 @@ async function registerAccount(req, res) {
   let nav = await utilities.getNav()
   const { account_firstname, account_lastname, account_email, account_password } = req.body
 
+  // Hash the password before storing
+  let hashedPassword
+  try {
+    // regular password and cost (salt is generated automatically)
+    hashedPassword = await bcrypt.hashSync(account_password, 10)
+  } catch (error) {
+    req.flash("notice", 'Sorry, there was an error processing the registration.')
+    return res.status(500).render("account/register", {
+      title: "Registration",
+      nav,
+      errors: null,
+    })
+  }
+
   const regResult = await accountModel.registerAccount(
     account_firstname,
     account_lastname,
     account_email,
-    account_password
+    hashedPassword
   )
 
   if (regResult) {
@@ -57,6 +75,7 @@ async function registerAccount(req, res) {
     })
   }
 }
+
 
 
 module.exports = {
