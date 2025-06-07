0f03e1a86b4663d62522cf5080259fd218d951da
ivan-pl809 <pon20007@byui.edu>
2025-06-06 20:49:56 -0600

    commit

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -2,12 +2,19 @@ const express = require("express")
 const router = new express.Router()
 const utilities = require("../utilities/")
 const accountController = require("../controllers/accountController")
+const regValidate = require('../utilities/account-validation')
+
 
 // Route for delivering login view
 router.get("/login", accountController.buildLogin)
 router.get("/register", accountController.buildRegister)
-router.post('/register', accountController.registerAccount)
-
+// Process the registration data
+router.post(
+  "/register",
+  regValidate.registationRules(),
+  regValidate.checkRegData,
+  accountController.registerAccount
+);
 
 
 module.exports = router
